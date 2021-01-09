360af35ed4d6d480c67b459b6790e4631de26b75
merve <merverbas40@gmail.com>
2021-01-10 00:57:54 +0300

    deneme

--- loop-controls.js
+++ loop-controls.js
@@ -8,13 +8,13 @@ evlenecegiYas=prompt("evlenme yaşını girin!");
  
 for(i=0; i<=35; i++){    
     
-    if(i == 0){
+    if(i === 0){
         console.log(ad + " yeni doğdu ");
     }
-    else if(i == 18){
+    else if(i === 18){
         console.log(ad  + " reşit oldu ");
     }
-    else if(i == evlenecegiYas){
+    else if(i === evlenecegiYas){
         console.log(ad + i + " yaşında evlendi ");
     }
     else {
